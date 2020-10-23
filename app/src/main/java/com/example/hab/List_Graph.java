8c75b1a14592efd130a86d2386824e818d5c637b
RiaFukuda <s1971007@s.konan-u.ac.jp>
2020-10-23 17:38:12 +0900

    もうむりです

--- app/src/main/java/com/example/hab/List_Graph.java
+++ app/src/main/java/com/example/hab/List_Graph.java
@@ -3,12 +3,23 @@ package com.example.hab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class List_Graph extends AppCompatActivity {
+
+    private EditText editText;
+    private TextView textView;
+    String date = MainActivity.date;
+    String naiyo = MainActivity.naiyo;
+    String kingaku = MainActivity.kingaku;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list__graph);
+
+        date = editText.getText().toString();
+        textView.setText(date);
     }
 }
