8105f14601f1eb7b4c48454bf052c860cf4a28d4
RiaFukuda <s1971007@s.konan-u.ac.jp>
2020-10-23 17:09:10 +0900

    がんばりました

--- app/src/main/java/com/example/hab/MainActivity.java
+++ app/src/main/java/com/example/hab/MainActivity.java
@@ -6,12 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private TextView textView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button etuButton = (Button)findViewById(R.id.Eturanbottun);
         etuButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -26,5 +32,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        editText = findViewById(R.id.Date);
+
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.Tourokubutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // エディットテキストのテキストを取得
+                String text = editText.getText().toString();
+                // 取得したテキストを TextView に張り付ける
+                textView.setText(text);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/hab/Other.java
+++ app/src/main/java/com/example/hab/Other.java
@@ -2,7 +2,10 @@ package com.example.hab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Other extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Other extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        Button sisyuButton = (Button)findViewById(R.id.Sisyutubutton);
+        sisyuButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Other.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
