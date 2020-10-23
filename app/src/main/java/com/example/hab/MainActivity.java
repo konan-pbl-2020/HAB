3b313288693623cfff74f3ad09fe031abab16d47
RiaFukuda <s1971007@s.konan-u.ac.jp>
2020-10-23 15:39:05 +0900

    ページを繋げましたあああ

--- app/src/main/java/com/example/hab/MainActivity.java
+++ app/src/main/java/com/example/hab/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.hab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button etuButton = (Button)findViewById(R.id.Eturanbottun);
+        etuButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,List_Graph.class);
+                startActivity(intent);
+            }
+        });
+        Button syuButton = (Button)findViewById(R.id.Syunyubutton);
+        syuButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Other.class);
+                startActivity(intent);
+            }
+        });
     }
 }
