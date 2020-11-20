a1c564e03b8b980e7e2ab0d7bd94762c6857037a
IwamotoKengo <s1971016@s.konan-u.ac.jp>
2020-11-20 13:36:48 +0900

    onegaisimasu

--- app/src/main/java/com/example/hab/MainActivity.java
+++ app/src/main/java/com/example/hab/MainActivity.java
@@ -22,13 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button etuButton = (Button)findViewById(R.id.Eturanbottun);
-        etuButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,List_Graph.class);
-                startActivity(intent);
-            }
-        });
         Button syuButton = (Button)findViewById(R.id.Syunyubutton);
         syuButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
