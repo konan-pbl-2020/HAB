ba01aac6eabe8b5b0a4bbc4bffafc297cb8ac96f
IwamotoKengo <s1971016@s.konan-u.ac.jp>
2020-11-20 13:27:19 +0900

    haihai

--- app/src/main/java/com/example/hab/MainActivity.java
+++ app/src/main/java/com/example/hab/MainActivity.java
@@ -22,13 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button etuButton = (Button)findViewById(R.id.Eturanbottun);
-        etuButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,List_Graph.class);
-                startActivity(intent);
-            }
-        });
         Button syuButton = (Button)findViewById(R.id.Syunyubutton);
         syuButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -51,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
                 date = editText.getText().toString();
                 naiyo = editText2.getText().toString();
                 kingaku = editText3.getText().toString();
+
+
+
 
                 Intent intent = new Intent(MainActivity.this,List_Graph.class);
                 startActivity(intent);
